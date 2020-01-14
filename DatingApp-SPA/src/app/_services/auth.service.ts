cc81306ecc6eba7766e1232fcf8f72b91150ceee
ivivanov18 <ivanov.ivan.v@gmail.com>
2020-01-14 08:13:27 +0200

    - add prettier.json config - add authService and hook it to be used by navbar login form

---
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthService {
+    baseUrl = 'http://localhost:5000/api/auth/';
+
+    constructor(private http: HttpClient) {}
+
+    login(model: any) {
+        console.log({ model });
+        return this.http.post(this.baseUrl + 'login', model).pipe(
+            map((response: any) => {
+                const user = response;
+                if (user && user.token) {
+                    localStorage.setItem('token', user.token);
+                }
+            })
+        );
+    }
+}
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 
+import { AuthService } from './_services/auth.service';
+
 @NgModule({
     declarations: [AppComponent, ValueComponent, NavComponent],
     imports: [BrowserModule, HttpClientModule, FormsModule],
-    providers: [],
+    providers: [AuthService],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
