c2102ad3d226a69b6cbd92461c39b2eb7ece3159
ivivanov18 <ivanov.ivan.v@gmail.com>
2020-01-15 23:30:06 +0200

    - remove values select component - add register auth service - hook it up to register method in register component

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
     selector: 'app-register',
@@ -8,12 +9,25 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 export class RegisterComponent implements OnInit {
     @Input() valuesFromHome: any;
     @Output() cancelRegister = new EventEmitter();
+    model: any = {};
 
-    constructor() {}
+    constructor(private authService: AuthService) {}
 
     ngOnInit() {}
 
     cancel() {
         this.cancelRegister.emit(false);
+    }
+
+    register() {
+        console.log(this.model);
+        this.authService.register(this.model).subscribe(
+            () => {
+                console.log('registration successful');
+            },
+            err => {
+                console.log({ err });
+            }
+        );
     }
 }
