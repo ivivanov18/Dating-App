61271af33ac10b9ded02331f652d484d8b7ad07c
ivivanov18 <ivanov.ivan.v@gmail.com>
2020-04-04 23:16:01 +0300

    ngx-bootstrap: - add ngx-bootstrap for dropdown loggedin menu - add logic to hide menu if user is loggedin

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -18,7 +20,13 @@ import { RegisterComponent } from './register/register.component';
         HomeComponent,
         RegisterComponent,
     ],
-    imports: [BrowserModule, HttpClientModule, FormsModule],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        BsDropdownModule.forRoot(),
+    ],
     providers: [ErrorInterceptorProvider, AuthService],
     bootstrap: [AppComponent],
 })
