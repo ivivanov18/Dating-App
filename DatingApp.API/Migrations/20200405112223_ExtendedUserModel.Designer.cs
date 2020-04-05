cda6765e5f2077d33c4b00e3a4789e55d3e0fd08
ivivanov18 <ivanov.ivan.v@gmail.com>
2020-04-05 14:28:01 +0300

    extend-api: - update user model to include more info - add Photo model - add migrations

--- DatingApp.API/Migrations/20200405112223_ExtendedUserModel.Designer.cs
+++ DatingApp.API/Migrations/20200405112223_ExtendedUserModel.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DatingApp.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200405112223_ExtendedUserModel")]
+    partial class ExtendedUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -41,7 +43,7 @@ namespace DatingApp.API.Migrations
 
                     b.HasIndex("UserId");
 
-                    b.ToTable("Photos");
+                    b.ToTable("Photo");
                 });
 
             modelBuilder.Entity("DatingApp.API.Models.User", b =>
