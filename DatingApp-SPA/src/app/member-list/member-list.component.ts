563dff3cbfcaf4c19bc9e2ecbf58d42d205a8155
ivivanov18 <ivanov.ivan.v@gmail.com>
2020-04-11 20:07:48 +0300

    member-list component: - add member-list component - add template - add loadUsers method

--- DatingApp-SPA/src/app/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/member-list/member-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { User } from '../_models/user';
 
 @Component({
-  selector: 'app-member-list',
-  templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+    selector: 'app-member-list',
+    templateUrl: './member-list.component.html',
+    styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent implements OnInit {
+    public users: User[];
+    constructor(
+        private userService: UserService,
+        private alertify: AlertifyService
+    ) {}
 
-  constructor() { }
+    ngOnInit() {
+        this.loadUsers();
+    }
 
-  ngOnInit() {
-  }
-
+    loadUsers() {
+        this.userService.getUsers().subscribe(
+            (users) => (this.users = users),
+            (error) => this.alertify.error(error)
+        );
+    }
 }
