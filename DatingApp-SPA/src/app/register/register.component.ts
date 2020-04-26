f65e31ca3de727d00a199f59252b35abf4a23125
ivivanov18 <ivanov.ivan.v@gmail.com>
2020-04-26 12:28:36 +0300

    register.component: - add BsDatepicker bootstrap component

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -7,6 +7,7 @@ import {
     Validators,
     FormBuilder,
 } from '@angular/forms';
+import { BsDatepickerConfig } from 'ngx-bootstrap';
 
 @Component({
     selector: 'app-register',
@@ -18,6 +19,7 @@ export class RegisterComponent implements OnInit {
     @Output() cancelRegister = new EventEmitter();
     // model: any = {};
     registerForm: FormGroup;
+    bsDatePickerConfig: Partial<BsDatepickerConfig>;
 
     constructor(
         private authService: AuthService,
@@ -27,6 +29,9 @@ export class RegisterComponent implements OnInit {
 
     ngOnInit() {
         this.createRegisterForm();
+        this.bsDatePickerConfig = {
+            containerClass: 'theme-red',
+        };
     }
 
     createRegisterForm() {
