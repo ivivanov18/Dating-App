50536ebbf8caa1ee4c853db4c4fdf3f45ee225dc
ivivanov18 <ivanov.ivan.v@gmail.com>
2020-05-17 17:06:02 +0300

    pagination: fix - fix itemsPerPage field

--- DatingApp.API/Helpers/PaginationHeader.cs
+++ DatingApp.API/Helpers/PaginationHeader.cs
@@ -4,14 +4,14 @@ namespace DatingApp.API.Helpers
     {
         public int TotalItems { get; set; }
         public int TotalPages { get; set; }
-        public int ItemsPerPages { get; set; }
+        public int ItemsPerPage { get; set; }
         public int CurrentPage { get; set; }
 
-        public PaginationHeader(int totalItems, int totalPages, int itemsPerPages, int currentPage)
+        public PaginationHeader(int totalItems, int totalPages, int itemsPerPage, int currentPage)
         {
             TotalItems = totalItems;
             TotalPages = totalPages;
-            ItemsPerPages = itemsPerPages;
+            ItemsPerPage = itemsPerPage;
             CurrentPage = currentPage;
         }
     }
